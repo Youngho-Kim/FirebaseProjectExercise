ec22f8a1100d05bee59c1a6c7337c016b18375d8
KWAVE\kwave <kwave55@gmail.com>
2017-07-04 19:31:40 +0900

    Firebase project 연습

---
+++ app/src/main/java/com/kwave/android/firebaseprojectexercise/SelectActivity.java
@@ -0,0 +1,19 @@
+package com.kwave.android.firebaseprojectexercise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("우리집");
+    }
+
+}
