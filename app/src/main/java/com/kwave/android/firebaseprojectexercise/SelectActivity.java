746aeba1dbed485c34635cb36dbf97f4ce2d64df
KWAVE\kwave <kwave55@gmail.com>
2017-07-04 21:42:01 +0900

    InformationActivity로 넘어가기 성공

--- app/src/main/java/com/kwave/android/firebaseprojectexercise/SelectActivity.java
+++ app/src/main/java/com/kwave/android/firebaseprojectexercise/SelectActivity.java
@@ -1,17 +1,31 @@
 package com.kwave.android.firebaseprojectexercise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SelectActivity extends AppCompatActivity {
+
+    ImageView imageInformation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
         setSupportActionBar(toolbar);
+
+        imageInformation = (ImageView) findViewById(R.id.imageInfomation);
+        imageInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectActivity.this,InfomationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
